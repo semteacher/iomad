5d23fc1ab2a235476fd235fac68f13d614b3b700
Andriy Semenets <semteacher@gmail.com>
2017-05-22 21:03:12 +0300

    cron task class files added

---
+++ mod/iomadcertificate/db/tasks.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Definition of Iomadcertificate expiration email scheduled tasks.
+ *
+ * @package   mod_iomadcertificate
+ * @category  task
+ * @copyright 2017 Semenets Andriy
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$tasks = array(
+    array(
+        'classname' => 'mod_iomadcertificate\task\expire_email_cron_task',
+        'blocking' => 0,
+        'minute' => '*/1',
+        'hour' => '*',
+        'day' => '*',
+        'month' => '*',
+        'dayofweek' => '*'
+    )
+    //array(
+    //    'classname' => 'local_email_reports\task\cron_task',
+    //    'blocking' => 0,
+    //    'minute' => '0',
+    //    'hour' => '0',
+    //    'day' => '*',
+    //    'month' => '*',
+    //    'dayofweek' => '*'
+    //)    
+);
